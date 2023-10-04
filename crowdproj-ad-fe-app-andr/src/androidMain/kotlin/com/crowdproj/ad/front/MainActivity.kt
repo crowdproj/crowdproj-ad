e60efdb499985d852e135a1577aea5a97b85ea85
Sergey Okatov <sokatov@gmail.com>
2023-10-04 12:19:31 +0500

    Android got shared frontlib

--- crowdproj-ad-fe-app-andr/src/androidMain/kotlin/com/crowdproj/ad/front/MainActivity.kt
+++ crowdproj-ad-fe-app-andr/src/androidMain/kotlin/com/crowdproj/ad/front/MainActivity.kt
@@ -1,10 +1,9 @@
 package com.crowdproj.ad.front
 
+import CwpAdMain
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
